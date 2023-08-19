16e51b31fb6edb8e670e6d79d5109bd122b3ece4
ada aykol <2031665@brunel.ac.uk>
2023-08-19 18:19:27 +0300

    refactored the landmarks, now the landmarks around iris are the only thing shown.

--- eye_movement_tracker/eye_movement_file.py
+++ eye_movement_tracker/eye_movement_file.py
@@ -6,13 +6,14 @@ face_mesh = mediapipe.solutions.face_mesh.FaceMesh(refine_landmarks=True)
 
 while True:                                             # infinete loop to keep the camera keep running
     _,frame = cam.read()                                # gets camera input
+    frame = cv2.flip(frame,1)                   # flips the camera view vertically so that its gives only the reflection on screen
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # this is turns the video into another color space
     output = face_mesh.process(rgb_frame)               # creates output from rgb_frame (required for face landmarks)
     landmark_points = output.multi_face_landmarks
     frame_height, frame_width, _ = frame.shape          # getting the frame_height and frame_width
     if landmark_points:
         landmarks = landmark_points[0].landmark                 # this detects only one face. (the first face that shows on camera)
-        for landmark in landmarks:                              #every landmark in the list landmarks
+        for landmark in landmarks[474:478]:                              #every landmark in the list landmarks but we only require the iris landmarks which are [474][478]
             x = int(landmark.x * frame_width)
             y = int(landmark.y * frame_height)
             cv2.circle(frame,(x,y), 3, (0, 255, 0))  #detects the landmarks of the face with green circles
