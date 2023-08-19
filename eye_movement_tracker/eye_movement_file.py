4e4d99645547b41319115f84f4235a276678763d
ada aykol <2031665@brunel.ac.uk>
2023-08-19 12:40:27 +0300

    created the landmark detector now the face movements are recognizable using OpenCV methods

--- eye_movement_tracker/eye_movement_file.py
+++ eye_movement_tracker/eye_movement_file.py
@@ -2,13 +2,20 @@ import cv2
 import mediapipe
 
 cam = cv2.VideoCapture(0)                               # this gets the first video capture device connected to machine (camera)
-face_mesh = mediapipe.solutions.face_mesh.FaceMesh()
+face_mesh = mediapipe.solutions.face_mesh.FaceMesh(refine_landmarks=True)
 
 while True:                                             # infinete loop to keep the camera keep running
     _,frame = cam.read()                                # gets camera input
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # this is turns the video into another color space
     output = face_mesh.process(rgb_frame)               # creates output from rgb_frame (required for face landmarks)
     landmark_points = output.multi_face_landmarks
-    print(landmark_points)
+    frame_height, frame_width, _ = frame.shape          # getting the frame_height and frame_width
+    if landmark_points:
+        landmarks = landmark_points[0].landmark                 # this detects only one face. (the first face that shows on camera)
+        for landmark in landmarks:                              #every landmark in the list landmarks
+            x = int(landmark.x * frame_width)
+            y = int(landmark.y * frame_height)
+            cv2.circle(frame,(x,y), 3, (0, 255, 0))  #detects the landmarks of the face with green circles
+            print(x, y)
     cv2.imshow('Eye Controlled Mouse', frame)   # creates a window to show video captures
     cv2.waitKey(1)
