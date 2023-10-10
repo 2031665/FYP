3722d05687b1d2887da2c50ea33728d2c0542aa6
ada aykol <2031665@brunel.ac.uk>
2023-10-10 19:51:12 +0100

    added the functionality to use with only eyes

--- eye_movement_tracker/eye_movement_file.py
+++ eye_movement_tracker/eye_movement_file.py
@@ -3,7 +3,8 @@ import mediapipe
 import pyautogui
 
 class EyeTracker:
-    def eye_track(self):
+    def eye_track(self):    #need to add the arguments isCameraUsed and isMuseUsed.
+        # if isCameraUsed:
         cam = cv2.VideoCapture(0)                                                        # this gets the first video capture device connected to machine (camera)
         face_mesh = mediapipe.solutions.face_mesh.FaceMesh(refine_landmarks=True)
         screen_width, screen_height = pyautogui.size()                                   # setting up the screen size so that the frame will match the screen size
@@ -16,17 +17,41 @@ class EyeTracker:
             frame_height, frame_width, _ = frame.shape                                   # getting the frame_height and frame_width
             if landmark_points:
                 landmarks = landmark_points[0].landmark                                  # this detects only one face. (the first face that shows on camera)
+                left =[landmarks[145], landmarks[159]]          #145-159
+                right = [landmarks[374], landmarks[386]]
                 for landmark in landmarks[473:474]:                                      # every landmark in the list landmarks but we only require the iris landmarks which are [474][478]
                     x = int(landmark.x * frame_width)
                     y = int(landmark.y * frame_height)
                     cv2.circle(frame,(x,y), 3, (0, 255, 0))            #detects the landmarks of the face with green circles
                     screen_x = int(landmark.x * screen_width)
                     screen_y = int(landmark.y * screen_height)
-                    print(screen_x, screen_y)  # test to see if the cursor matches the correct pixels on the computer
+                    #print(screen_x, screen_y)  # test to see if the cursor matches the correct pixels on the computer
                     data = pyautogui.moveTo(screen_x, screen_y)
+            #if isMusedUsed
+                for landmark in left:
+                    x = int(landmark.x * frame_width)
+                    y = int(landmark.y * frame_height)
+                    cv2.circle(frame, (x, y), 3, (0, 255, 255))
+                for landmark in right:
+                    x = int(landmark.x * frame_width)
+                    y = int(landmark.y * frame_height)
+                    cv2.circle(frame, (x, y), 3, (0, 255, 255))
+
+                if (right[0].y - right[1].y) < 0.004 and (left[0].y - left[1].y) < 0.004:
+                    print("noclick")
+                    pyautogui.sleep(0.2)
+                else:
+                    if(right[0].y - right[1].y) < 0.004:
+                        pyautogui.rightClick()
+                        print("right click")
+                        pyautogui.sleep(0.2)
+                    if (left[0].y - left[1].y) < 0.004:
+                        pyautogui.leftClick()
+                        print("left click")
+                        pyautogui.sleep(0.2)
             cv2.imshow('Eye Controlled Mouse', frame)                            # creates a window to show video captures
             cv2.waitKey(1)
 
-# if __name__ == "__main__":
-#     Run = EyeTracker()
-#     Run.eye_track()
+if __name__ == "__main__":
+    Run = EyeTracker()
+    Run.eye_track()
